24ea4970cd172d7599b5f77e3a60f7788f2e8f3c
Jeff Weisman <jweisman11@gmail.com>
2024-08-18 10:06:23 -0400

    update

--- src/hello.py
+++ src/hello.py
@@ -2,8 +2,13 @@ def say_hello(name: str) -> str:
     """
     docstring
     """
+    a = {"1": 1, "test": "elephant"}
+
+    v = {"1": 1, "test": "elephant"}
 
     return "Hello " + name
 
+
+other = a
 
 a = x
